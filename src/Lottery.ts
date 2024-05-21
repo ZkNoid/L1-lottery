8fd378b395eedc68c8e3b9fed5d0cd54cf0cbd8e
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-05-21 11:21:54 +0300

    Added field and function declarations

--- src/Lottery.ts
+++ src/Lottery.ts
@@ -1,7 +1,25 @@
 import { Field, SmartContract, state, State, method } from 'o1js';
 
 export class Lottery extends SmartContract {
+  @state(Field) ticketRoot = State<Field>();
+  @state(Field) ticketAmountRoot = State<Field>();
+  @state(Field) roundResultRoot = State<Field>();
+
   init() {
     super.init();
+
+    // #TODO Permisions
+  }
+
+  @method async buyTicket(ticketHash: Field, amount: Field) {
+    // #TODO
+  }
+
+  @method async produceResult(round: Field) {
+    // #TODO
+  }
+
+  @method async getReward(ticketHash: Field) {
+    // #TODO
   }
 }
