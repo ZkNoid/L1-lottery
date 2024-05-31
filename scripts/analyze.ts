71f7809b323ac2896d5979244cb7f9c6e208bee4
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-05-31 20:32:25 +0300

    Added analyze script and initial calculations

--- scripts/analyze.ts
+++ scripts/analyze.ts
@@ -1,8 +1,25 @@
 import { Lottery } from '../src/Lottery.js';
 import { DistibutionProgram } from '../src/DistributionProof.js';
+import { writeFileSync } from 'fs';
 
-console.log(await Lottery.analyzeMethods());
-console.log(await DistibutionProgram.analyzeMethods());
+const lotteryResult = await Lottery.analyzeMethods();
+const distributionResult = await DistibutionProgram.analyzeMethods();
 
-// console.log(DistibutionProgram.analyzeMethods());
-// console.log(Lottery.analyzeMethods());
+if (!Lottery._methods) {
+  console.log("Can't find methods for Lottery");
+  throw new Error("Can't find methods for Lottery");
+}
+
+let result: { [name: string]: number } = {};
+
+for (const method of Lottery._methods) {
+  result[`Lottery_${method.methodName}`] =
+    lotteryResult[method.methodName].rows;
+}
+
+result[`DistributionProof_init`] = distributionResult.init.rows;
+result[`DistibutionProof_addTicket`] = distributionResult.addTicket.rows;
+
+console.log(result);
+
+writeFileSync('analyze_result.json', JSON.stringify(result, null, 2));
