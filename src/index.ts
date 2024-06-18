60215987c99f49e2c4038bc38af3be60587f310e
asimaranov <a.simaranov@gmail.com>
2024-06-18 17:32:22 +0300

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import { Lottery } from './Lottery.js';
+import { Lottery, comisionTicket, getNullifierId } from './Lottery.js';
 import { Ticket } from './Ticket';
-import { DistibutionProgram } from './DistributionProof.js';
+import { DistibutionProgram, DistributionProofPublicInput } from './DistributionProof.js';
+import { NumberPacked, getEmpty2dMerkleMap } from './util.js';
+import { MerkleMap20, MerkleMap20Witness } from './CustomMerkleMap.js';
+import { TICKET_PRICE } from './constants.js';
 
-export { Ticket, Lottery, DistibutionProgram };
+export {
+  Ticket,
+  Lottery,
+  DistibutionProgram,
+  getEmpty2dMerkleMap,
+  getNullifierId,
+  MerkleMap20,
+  MerkleMap20Witness,
+  TICKET_PRICE,
+  NumberPacked,
+  DistributionProofPublicInput,
+  comisionTicket
+};
