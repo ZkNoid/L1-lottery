fe578e63ba5042830f29ea65d80b41e2134fa338
asimaranov <a.simaranov@gmail.com>
2024-06-18 14:06:15 +0300

    Updated package name & exported distribution program

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Lottery } from './Lottery.js';
 import { Ticket } from './Ticket';
+import { DistibutionProgram } from './DistributionProof.js';
 
-export { Ticket, Lottery };
+export { Ticket, Lottery, DistibutionProgram };
