46ec5d344423f7898772faba0552c4d0eb077058
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-07-19 16:30:27 +0300

    Changed generate cache cycle

--- scripts/generate_cache.ts
+++ scripts/generate_cache.ts
@@ -3,7 +3,7 @@ import { DistibutionProgram } from '../src/DistributionProof.js';
 import { PLottery } from '../src/PLottery.js';
 import { TicketReduceProgram } from '../src/TicketReduceProof.js';
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 1; i++) {
   console.log(`${i} run`);
   await DistibutionProgram.compile({ cache: Cache.FileSystem('./cache') });
   await TicketReduceProgram.compile({ cache: Cache.FileSystem('./cache') });
