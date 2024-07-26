29f1149f22d6c8a1e43a7f3c2143d6a217e73b42
asimaranov <a.simaranov@gmail.com>
2024-07-26 23:00:01 +0300

    Cleaned bug poc

--- scripts/bug_poc.ts
+++ scripts/bug_poc.ts
@@ -8,8 +8,9 @@ const Network = Mina.Network({
   mina: 'https://api.minascan.io/node/devnet/v1/graphql',
   archive: 'https://api.minascan.io/archive/devnet/v1/graphql',
 });
-// const Network = Mina.Network(config.url);
+
 Mina.setActiveInstance(Network);
+
 let lottery = new PLottery(
   PublicKey.fromBase58(
     'B62qk3U9ZUZ2a21T4thqhUgBvXM3xMyA1HtQBQ79unUHrytEVpNhtqm'
