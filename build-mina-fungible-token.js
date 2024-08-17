48ed6b721c927a5a085dd77fa6d5f71049ca45a5
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-08-17 12:55:34 +0300

    Added token build script

---
+++ build-mina-fungible-token.js
@@ -0,0 +1,8 @@
+// Taken from here https://github.com/ZKON-Network/zkapp/blob/devnet/build-mina-fungible-token.js
+
+import * as fs from 'fs';
+
+const configFile = 'node_modules/mina-fungible-token/tsconfig.json';
+const content = JSON.parse(fs.readFileSync(configFile));
+delete content.compilerOptions.typeRoots;
+fs.writeFileSync(configFile, JSON.stringify(content, null, 4));
