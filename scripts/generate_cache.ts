be61c70c1a179d3ae743be0fe2fd9308c850aff8
aii23 <a.ivlev.om@gmail.com>
2024-10-14 15:37:12 +0700

    Updated round duration to 2 hours

--- scripts/generate_cache.ts
+++ scripts/generate_cache.ts
@@ -1,39 +1,39 @@
-// import * as fs from 'fs';
-// import { Cache, PublicKey } from 'o1js';
-// import { DistributionProgram } from '../src/Proofs/DistributionProof.js';
-// import { TicketReduceProgram } from '../src/Proofs/TicketReduceProof.js';
-// import { getPLottery } from '../src/PLottery.js';
-// import { getRandomManager } from '../src/Random/RandomManager.js';
+import * as fs from 'fs';
+import { Cache, PublicKey } from 'o1js';
+import { DistributionProgram } from '../src/Proofs/DistributionProof.js';
+import { TicketReduceProgram } from '../src/Proofs/TicketReduceProof.js';
+import { RandomManager } from '../src/Random/RandomManager.js';
+import { PLottery } from '../src/PLottery.js';
+import { ZkonRequestCoordinator, ZkonZkProgram } from 'zkon-zkapp';
 
-// // If no epoch is provided - last one will be used
-// let deploy_epoch = process.argv[2] ? process.argv[2] : 'current';
+// If no epoch is provided - last one will be used
+let deploy_epoch = process.argv[2] ? process.argv[2] : 'current';
 
-// let addressesBuffer = fs.readFileSync(
-//   `./deploy/addresses/${deploy_epoch}.json`
-// );
-// let addresses: {
-//   randomManagerAddress: string;
-//   lotteryAddress: string;
-//   randomManagerOwner: string;
-// } = JSON.parse(addressesBuffer.toString());
+let addressesBuffer = fs.readFileSync(
+  `./deploy/addresses/${deploy_epoch}.json`
+);
+let addresses: {
+  randomManagerAddress: string;
+  lotteryAddress: string;
+  randomManagerOwner: string;
+} = JSON.parse(addressesBuffer.toString());
 
-// let randomManagerAddress = PublicKey.fromBase58(addresses.randomManagerAddress);
-// let lotteryAddress = PublicKey.fromBase58(addresses.lotteryAddress);
-// let randomManagerOwner = PublicKey.fromBase58(addresses.randomManagerOwner);
+let randomManagerAddress = PublicKey.fromBase58(addresses.randomManagerAddress);
+let lotteryAddress = PublicKey.fromBase58(addresses.lotteryAddress);
+let randomManagerOwner = PublicKey.fromBase58(addresses.randomManagerOwner);
 
-// await DistributionProgram.compile({ cache: Cache.FileSystem('./cache/DP') });
-// await TicketReduceProgram.compile({ cache: Cache.FileSystem('./cache/TRP') });
+await DistributionProgram.compile({ cache: Cache.FileSystem('./cache/DP') });
+await TicketReduceProgram.compile({ cache: Cache.FileSystem('./cache/TRP') });
 
-// let RandomManager = getRandomManager(randomManagerOwner);
+await ZkonZkProgram.compile({ cache: Cache.FileSystem('cache') });
+await ZkonRequestCoordinator.compile({ cache: Cache.FileSystem('cache') });
 
-// let PLottery = getPLottery(randomManagerAddress, randomManagerOwner);
+await RandomManager.compile({
+  cache: Cache.FileSystem(
+    `./cache/RandomManager/${addresses.randomManagerAddress}`
+  ),
+});
 
-// await RandomManager.compile({
-//   cache: Cache.FileSystem(
-//     `./cache/RandomManager/${addresses.randomManagerAddress}`
-//   ),
-// });
-
-// await PLottery.compile({
-//   cache: Cache.FileSystem(`./cache/PLottery/${addresses.lotteryAddress}`),
-// });
+await PLottery.compile({
+  cache: Cache.FileSystem(`./cache/PLottery/${addresses.lotteryAddress}`),
+});
