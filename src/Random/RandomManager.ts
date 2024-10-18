949cedc805914dd2feb8fd7e8f7e952003057a75
aii23 <a.ivlev.om@gmail.com>
2024-10-18 17:00:04 +0700

    Removed random value computed check

--- src/Random/RandomManager.ts
+++ src/Random/RandomManager.ts
@@ -100,7 +100,7 @@ export class RandomManager extends SmartContract {
     // Check VRF computed
     const curRandomValue = this.curRandomValue.getAndRequireEquals();
     // Check is ommitted for a while
-    curRandomValue.assertGreaterThan(Field(0), 'reveal: No random value');
+    // curRandomValue.assertGreaterThan(Field(0), 'reveal: No random value');
 
     // Check commit
     const commit = this.commit.getAndRequireEquals();
